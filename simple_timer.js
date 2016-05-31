f3e64e6482a24a25415ebfd90d4b1a7ac6390d91
wangshuang <shuang.wang0909@gmail.com>
2016-05-31 15:18:16 +0800

    simple_timer

---
+++ simple_timer.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wangshuang on 16/5/31.
+ */
+function simpleTimeout(consoleTimer){
+    console.timeEnd(consoleTimer)
+}
+
+console.time("twoSecond");
+setTimeout(simpleTimeout, 2000, "twoSecond");
+console.time("oneSecond");
+setTimeout(simpleTimeout, 1000, "oneSecond");
+console.time("fiveSecond");
+setTimeout(simpleTimeout, 5000, "fiveSecond");
+console.time("50MilliSecond");
+setTimeout(simpleTimeout, 50, "50MilliSecond");
